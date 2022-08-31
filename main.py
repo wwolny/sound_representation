87a87cf612de86a4a6680794e5fdea7641f30079
wwolny <wwolny1@gmail.com>
2022-08-31 15:05:48 +0200

    run_experiments

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import logging
 import argparse
 import numpy as np
 from typing import List
+from dataclasses import asdict
 import matplotlib.pyplot as plt
 from torchaudio.datasets import LJSPEECH
 from torchmetrics import SignalNoiseRatio
@@ -14,6 +15,7 @@ from torchaudio.transforms import MelSpectrogram, AmplitudeToDB
 
 from sound_repr.dataset import Samples, CustomDataset
 from sound_repr.utils import Sine, LSD
+from cfg import MainConfig
 
 logger = logging.getLogger()
 logging.basicConfig(
@@ -28,67 +30,60 @@ def parse_arguments(args):
     return parser.parse_args(args)
 
 
-def main():
-    args = parse_arguments(sys.argv[1:])
-
-    # Load config file
-    logger.info("Loading config...")
-    with open(args.config_file) as f:
-        config = yaml.safe_load(f)
-
-    logger.info("Setting seed {0}".format(config["seed"]))
-    torch.manual_seed(config["seed"])
+def main(config: MainConfig):
+    logger.info("Setting seed {0}".format(config.seed))
+    torch.manual_seed(config.seed)
 
     logger.info("Process dataset...")
-    if config["name"] == "LJSPEECH":
-        dataset = LJSPEECH('drive/MyDrive/MgrData', download=False)
-    elif config["name"] == "GTZAN":
+    if config.name == "LJSPEECH":
+        dataset = LJSPEECH('datasets', download=False)
+    elif config.name == "GTZAN":
         # dataset = GTZAN('drive/MyDrive/MgrData', download=True)
-        dataset = CustomDataset("drive/MyDrive/MgrData/small_gtzan/wavs")
-    elif config["name"] == "ESC50":
-        dataset = CustomDataset("drive/MyDrive/MgrData/small_rsc_50/wavs")
-    elif config["name"] == "UrbanSound":
-        dataset = CustomDataset("drive/MyDrive/MgrData/small_urban_sound/wavs")
+        dataset = CustomDataset("datasets/small_gtzan/wavs")
+    elif config.name == "ESC50":
+        dataset = CustomDataset("datasets/small_rsc_50/wavs")
+    elif config.name == "UrbanSound":
+        dataset = CustomDataset("datasets/small_urban_sound/wavs")
     else:
         logger.error("Wrong dataset name")
         return
 
-    config["SR"] = dataset[0][1]
+    config.SR = dataset[0][1]
 
     melspec = MelSpectrogram(
-        sample_rate=config["SR"],
-        n_fft=config["n_fft"],
-        hop_length=config["hop_length"],
-        n_mels=config["n_mels"]
+        sample_rate=config.SR,
+        n_fft=config.n_fft,
+        hop_length=config.hop_length,
+        n_mels=config.n_mels
     )
     amp2db = AmplitudeToDB()
 
-    samples = Samples(dataset, SR=config["SR"], frame_len=config["frame_len"])
+    samples = Samples(dataset, SR=config.SR, frame_len=config.frame_len)
 
-    logger.info("Running in mode: {0}".format(config["mode"]))
-    for id_ in range(config["samples"]):
+    logger.info("Running in mode: {0}".format(config.mode))
+    for id_ in range(config.samples):
         # Initialize wandb
-        if config["wandb"]:
+        if config.wandb:
             logger.info("Initializing  WandB...")
             run = wandb.init(
                 project="sound_representation",
                 entity="wwolny",
-                config={**config, **{"sample_id": id_}},
+                config={**asdict(config), **{"sample_id": id_}},
                 reinit=True,
             )
 
         logger.info(f"Process sample index: {id_}.")
         data = samples[id_]
         x_numpy = np.array(list(map(lambda el: [el], range(len(data)))))
-        x_numpy = (x_numpy / len(x_numpy)) * 2 * config["t_scale_max"] - config["t_scale_max"]
+        x_numpy = (x_numpy / len(x_numpy)) * 2 * config.t_scale_max - config.t_scale_max
         x = torch.from_numpy(x_numpy).float()
 
         y_numpy = np.array(list(map(lambda el: [el], data)))
         y_numpy = (y_numpy / max(max(y_numpy), -1 * min(y_numpy)))
         y = torch.from_numpy(y_numpy).float()
 
-        if config["mode"] == "nerf":
-            L = config["L"]
+        if config.mode == "nerf":
+            L = config.L
             indices = torch.arange(0, len(x_numpy),
                                    dtype=torch.float).unsqueeze(-1)
             indices = -1 + (1 + 1) * indices / (len(x_numpy) - 1)
@@ -103,84 +98,84 @@ def main():
                 *build_network(
                     2 * L,
                     1,
-                    config["network"],
+                    config.network,
                     torch.nn.ReLU(),
-                    config["bias"],
+                    config.bias,
                     torch.nn.ReLU(),
                 )
             )
-        elif config["mode"] == "siren":
+        elif config.mode == "siren":
             module_lst = build_network(
                 1,
                 1,
-                config["network"],
-                Sine(config["hidden_omega"]),
-                config["bias"],
-                Sine(config["omega"]),
+                config.network,
+                Sine(config.hidden_omega),
+                config.bias,
+                Sine(config.omega),
             )
             with torch.no_grad():
                 for id_mod, mod in enumerate(module_lst):
                     if isinstance(mod, torch.nn.Linear):
                         if id_mod == 0:
                             mod.weight.uniform_(-1 / mod.in_features, 1 / mod.in_features)
-                            if config["siren_bias_init"]:
+                            if config.siren_bias_init:
                                 mod.bias.uniform_(-1 / mod.in_features, 1 / mod.in_features)
                         else:
                             mod.weight.uniform_(
-                                -np.sqrt(6 / mod.in_features) / config["hidden_omega"],
-                                np.sqrt(6 / mod.in_features) / config["hidden_omega"]
+                                -np.sqrt(6 / mod.in_features) / config.hidden_omega,
+                                np.sqrt(6 / mod.in_features) / config.hidden_omega
                             )
-                            if config["siren_bias_init"]:
+                            if config.siren_bias_init:
                                 mod.bias.uniform_(
-                                    -np.sqrt(6 / mod.in_features) / config["hidden_omega"],
-                                    np.sqrt(6 / mod.in_features) / config["hidden_omega"]
+                                    -np.sqrt(6 / mod.in_features) / config.hidden_omega,
+                                    np.sqrt(6 / mod.in_features) / config.hidden_omega
                                 )
             model = torch.nn.Sequential(
                 *module_lst
             )
 
-        elif config["mode"] == "default":
+        elif config.mode == "default":
             model = torch.nn.Sequential(
                 *build_network(
                     1,
                     1,
-                    config["network"],
+                    config.network,
                     torch.nn.ReLU(),
-                    config["bias"],
+                    config.bias,
                     torch.nn.ReLU(),
                 )
             )
         else:
             exit()
 
-        if config["wandb"]:
+        if config.wandb:
             run.watch(model)
 
-        loss_fn = getattr(torch.nn, config["loss_fn"])()
-        optimizer = getattr(torch.optim, config["optimizer"])(
-            model.parameters(), lr=config["lr"])
+        loss_fn = getattr(torch.nn, config.loss_fn)()
+        optimizer = getattr(torch.optim, config.optimizer)(
+            model.parameters(), lr=config.lr)
 
         plot_spectrogram(
             sample=y.detach().numpy(),
             mel_spec=melspec,
             amp2db=amp2db
         )
-        if config["wandb"]:
+        if config.wandb:
             run.log({"original": plt}, commit=False)
         else:
             plt.show()
         plt.close()
 
         # train
-        batch_size = config["batch_size"]
+        batch_size = config.batch_size
         loss_fn_list = []
         loss_lst = {}
-        for el in config["loss_lst"]:
+        for el in config.loss_lst:
             loss_lst[el] = getattr(torch.nn, el)()
-        if config["SNR"]:
+        if config.SNR:
             loss_lst["SNR"] = SignalNoiseRatio()
-        for i in range(config["epochs"]):
-            if config["batched"]:
+        for i in range(config.epochs):
+            if config.batched:
                 permutation = torch.randperm(x.size()[0])
                 for j in range(0, x.size()[0], batch_size):
                     optimizer.zero_grad()
@@ -195,7 +190,7 @@ def main():
                 y_pred = model(x)
                 loss = loss_fn(y_pred, y)
                 loss_fn_list.append(loss.item())
-                if config["wandb"]:
+                if config.wandb:
                     for el in loss_lst.keys():
                         run.log({el: loss_lst[el](y_pred, y).item(), 'epoch': i}, commit=False)
             else:
@@ -204,18 +199,18 @@ def main():
                 y_pred = model(x)
                 loss = loss_fn(y_pred, y)
                 loss_fn_list.append(loss.item())
-                if config["wandb"]:
+                if config.wandb:
                     for el in loss_lst.keys():
                         run.log({el: loss_lst[el](y_pred, y).item(), 'epoch': i}, commit=False)
 
                 loss.backward()
                 optimizer.step()
 
-            if config["plot"] and i % config["log_img"] == 0:
+            if config.plot and i % config.log_img == 0:
                 y_pred = model(x)
                 plt.plot(y)
                 plt.plot(y_pred.detach().numpy())
-                if config["wandb"]:
+                if config.wandb:
                     run.log({"wave": plt}, commit=False)
                 else:
                     plt.show()
@@ -225,16 +220,16 @@ def main():
                     mel_spec=melspec,
                     amp2db=amp2db
                 )
-                if config["wandb"]:
+                if config.wandb:
                     run.log({"spectrogram": plt}, commit=False)
                 else:
                     plt.show()
                 plt.close()
-            if config["wandb"]:
+            if config.wandb:
                 run.log({'loss': loss.item(), 'epoch': i})
         logger.info(
             "Sample {0} final loss equals: {1}".format(id_, loss_fn_list[-1]))
-        if config["wandb"]:
+        if config.wandb:
             run.finish()
 
 
@@ -270,4 +265,11 @@ def plot_spectrogram(sample, mel_spec, amp2db):
 
 
 if __name__ == '__main__':
-    main()
+    args = parse_arguments(sys.argv[1:])
+
+    # Load config file
+    logger.info("Loading config...")
+    with open(args.config_file) as f:
+        config = yaml.safe_load(f)
+    main_config = MainConfig.from_dict(config)
+    main(main_config)
---
+++ run_experiments.py
@@ -0,0 +1,77 @@
+from main import main
+from cfg import MainConfig
+
+main_cfg = MainConfig()
+
+models = ["default", "nerf", "siren"]
+
+# Nerf test różnych L
+test_l = [1, 16, 32, 64]
+
+# wielkość sieci default
+network_def = [[512, 512, 512, 512, 512]]
+
+# wielkość sieci nerf
+network_nerf = [[128, 128, 128, 128, 128]]
+
+# wielkość sieci siren
+network_sir = [[128, 128, 128, 128, 128]]
+
+# scale_input
+scale_def = [1]
+scale_nerf = [1]
+scale_siren = [1, 300, 600]
+
+# omega
+omega_0 = [1, 30, 50, 100, 1000]
+omega_hidden = [1, 30, 50, 100, 1000]
+
+if __name__ == '__main__':
+    samples = 1
+    epochs = 1000
+
+    model = "default"
+    nets = network_def
+    scale = scale_def
+    def_cfg = MainConfig.from_dict({
+        "samples": samples,
+        "epochs": epochs,
+        "mode": model,
+        "network": nets[0],
+        "t_scale_max": scale[0],
+    })
+    main(def_cfg)
+
+    model = "nerf"
+    nets = network_nerf
+    scale = scale_nerf
+    L_s = test_l
+    for net in nets:
+        for test_l in test_l:
+            nerf_cfg = MainConfig.from_dict({
+                "samples": samples,
+                "epochs": epochs,
+                "mode": model,
+                "network": net,
+                "t_scale_max": scale[0],
+                "L": test_l
+            })
+            main(nerf_cfg)
+
+    model = "siren"
+    nets = network_sir
+    scale = scale_siren
+    for net in nets:
+        for sca in scale:
+            for o_0 in omega_0:
+                for h_o in omega_hidden:
+                    siren_cfg = MainConfig.from_dict({
+                        "samples": samples,
+                        "epochs": epochs,
+                        "mode": model,
+                        "network": net,
+                        "t_scale_max": sca,
+                        "omega": o_0,
+                        "hidden_omega": h_o
+                    })
+                    main(siren_cfg)
